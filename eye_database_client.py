efd27a95a9413f792824112c71cd42556b1b52c5
Jenny Xin <jingyi.xin@duke.edu>
2021-10-24 09:55:27 -0400

    edited url to the one for the virtual machine

--- eye_database_client.py
+++ eye_database_client.py
@@ -2,11 +2,11 @@ import requests
 
 
 driver1 = {"username": "Dave", "time_blinking": 1, "too_sleepy": False}
-r = requests.post("http://127.0.0.1:5000/new_user", json=driver1)
+r = requests.post("http://vcm-23122.vm.duke.edu/new_user", json=driver1)
 print(r.status_code)
 print(r.text)
 
 name = "Betty"
-r = requests.get("http://127.0.0.1:5000/blink", json=name)
+r = requests.get("http://vcm-23122.vm.duke.edu/blink", json=name)
 print(r.status_code)
 print(r.text)

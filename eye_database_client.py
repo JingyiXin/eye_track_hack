ca8308c62de3d745043f68eb9d52454a46782b77
Jenny Xin <jingyi.xin@duke.edu>
2021-10-24 12:09:01 -0400

    added eye aspect ratio data to database

--- eye_database_client.py
+++ eye_database_client.py
@@ -1,12 +1,12 @@
 import requests
 
 
-driver1 = {"username": "Dave", "time_blinking": 1, "too_sleepy": False}
-r = requests.post("http://vcm-23122.vm.duke.edu/new_user", json=driver1)
+driver1 = {"username": "Bob", "time_blinking": 2, "too_sleepy": False}
+r = requests.post("http://vcm-23122.vm.duke.edu:5000/new_user", json=driver1)
 print(r.status_code)
 print(r.text)
 
 name = "Betty"
-r = requests.get("http://vcm-23122.vm.duke.edu/blink", json=name)
+r = requests.get("http://vcm-23122.vm.duke.edu:5000/sleepy", json=name)
 print(r.status_code)
 print(r.text)
